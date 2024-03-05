d9f9e73c318ee780d9ab604d9b8ceb6c47a70b8a
sbaksh4527 <78521742+sbaksh4527@users.noreply.github.com>
2024-03-04 22:55:45 -0500

    Completing HW 05 Part 1

--- Tester.py
+++ Tester.py
@@ -1,12 +1,20 @@
+"""
+    Module is importing unittest
+
+"""
 import unittest
 from unittest.mock import Mock, patch
-from assignment_Part_one import get_user_repositories  
+from assignment_part_one import get_user_repositories
 
 
 class TestGetUserRepositories(unittest.TestCase):
+    """
+    Class is utilized to set up the various functions following 
 
+    """
     @patch('requests.get')
     def test_get_user_repositories_success(self, mock_get):
+        """Function is  checking if it can successfully retrieve user repositories."""
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = [{'name': 'repo1'}, {'name': 'repo2'}]
 
@@ -16,6 +24,7 @@ class TestGetUserRepositories(unittest.TestCase):
 
     @patch('requests.get')
     def test_get_user_repositories_failed_request(self, mock_get):
+        """Function is checking if the user repository request failed """
         mock_get.return_value.status_code = 404
 
         repos_with_commits = get_user_repositories('test_user')
@@ -23,6 +32,7 @@ class TestGetUserRepositories(unittest.TestCase):
 
     @patch('requests.get')
     def test_get_user_repositories_failed_commits_request(self, mock_get):
+        """Function is checking if the user repository commit request failed"""
         mock_get.side_effect = [
             Mock(status_code=200, json=lambda: [{'name': 'repo1'}]),
             Mock(status_code=404)
--- assignment_Part_one.py
+++ assignment_Part_one.py
@@ -1,22 +1,27 @@
+"""
+    Module is utilizing the github API 
+
+"""
 import requests
 def get_user_repositories(user_id):
-  
+    """ Function is getting user repositories """
+
     url = f"https://api.github.com/users/{user_id}/repos"
 
-    
+
     response = requests.get(url)
 
-   
+
     if response.status_code == 200:
-        repositories = response.json()  
+        repositories = response.json()
         repo_commits = []
 
-        
+
         for repo in repositories:
             repo_name = repo['name']
             commits_url = f"https://api.github.com/repos/{user_id}/{repo_name}/commits"
 
-            
+
             commits_response = requests.get(commits_url)
             if commits_response.status_code == 200:
                 commits = commits_response.json()
@@ -25,14 +30,16 @@ def get_user_repositories(user_id):
 
         return repo_commits
     else:
-        
-        print(f"Failed to retrieve repositories for user '{user_id}'. Error: {response.status_code}")
+
+        print(f"Failed to grab repositories for user '{user_id}'. Error: {response.status_code}")
 
 
 user_id = "sid4527"
 repos_with_commits = get_user_repositories(user_id)
+
 if repos_with_commits:
     for repo, num_commits in repos_with_commits:
         print(f"Repo: {repo} Number of commits: {num_commits}")
 else:
-    print("No repositories found or an error occurred.")
+        print("No repositories found or an error occurred.")
+    
